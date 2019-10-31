7fa7d533d8d2f7be0a717329134060c93e3bdecb
quincyreid <qreid51@gmail.com>
2019-10-30 21:29:43 -0400

    Create express server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT || 3000, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
