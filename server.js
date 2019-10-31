d1f9bcaa95367f383fc3cda9d5c8dbb9a4be4bdd
quincyreid <qreid51@gmail.com>
2019-10-30 22:22:06 -0400

    Install yelp-fusion module and create post route used for submitting form

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+const yelp = require("yelp-fusion");
+const client = yelp.client(process.env.APIKEY);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(request, response) {
   response.render(__dirname + "/views/index.ejs");
 });
+
+app.post("/florists", (request, response) => {
+  client.search({
+    term: 'florists',
+    location: request.body.florists,
+  })
+  .then(res => {
+    let data = res.jsonBody;
+    response.render(__dirname + "/views/florist.ejs", {data: data});  })
+  .catch(e => console.log(e));
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, function() {
