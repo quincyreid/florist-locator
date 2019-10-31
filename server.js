fdc10079866b7b884c1967e26a64ae72b6f865d0
quincyreid <qreid51@gmail.com>
2019-10-30 21:59:01 -0400

    Install view engine and render index page

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.get("/", function(request, response) {
+  response.render(__dirname + "/views/index.ejs");
+});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, function() {
   console.log("Your app is listening on port " + listener.address().port);
